291b5675a552b9439c8e2d787e17052c09888f09
Lnrsx <69937345+Lnrsx@users.noreply.github.com>
2021-12-29 11:35:30 +0000

    Merged player and dealer drawer

--- src/game.js
+++ src/game.js
@@ -30,35 +30,42 @@ gamestate = {
     dealer: ['4C', '3H', '5D'],
 }
 
-// changes hand achor so that hand is always horizontally aligned
-playerhandstartpoint = anchors.player.posX - (gamestate.player.length * cardsize.width / 2)
-gamestate.player.forEach(function (card, index) {
-    var cardimage = new Image();
-    cardimage.src = `../app/cards100/${card}.png`
+function drawgamestate(playeroffset = 0, dealeroffset = 0) {
 
-    cardimage.onload = function() {
+    // draws the hand of the player and dealer (or every entry in the gamestate object)
+    for (const [unit, hand] of Object.entries(gamestate)) {
+        // changes hand achor so that hand is always horizontally aligned
+        // offset of the hand can be added if a card is being animated towards the hand and needs room
+        handstartpoint = anchors[unit].posX - ((hand.length + playeroffset) * cardsize.width / 2)
+        gamestate.player.forEach(function (card, index) {
+            var cardimage = new Image();
+            cardimage.src = `../app/cards100/${card}.png`
+
+            cardimage.onload = function() {
+                ctx.drawImage(
+                    cardimage,
+                    handstartpoint + (cardsize.width * index),
+                    anchors[unit].posY,
+                    cardsize.width,
+                    cardsize.height,
+                )
+            }
+        })
+    }
+
+    // Draws deck image (will stay static during game)
+    let deckimage = new Image();
+    deckimage.src = '../app/cards100/deck.png'
+
+    deckimage.onload = function () {
         ctx.drawImage(
-            cardimage,
-            playerhandstartpoint + (cardsize.width * index),
-            anchors.player.posY,
+            deckimage,
+            anchors.deck.posX,
+            anchors.deck.posY,
             cardsize.width,
-            cardsize.height,
+            cardsize.height
         )
     }
-})
+}
 
-dealerhandstartpoint = anchors.dealer.posX - (gamestate.dealer.length * cardsize.width / 2)
-gamestate.dealer.forEach(function (card, index) {
-    var cardimage = new Image();
-    cardimage.src = `../app/cards100/${card}.png`
-
-    cardimage.onload = function() {
-        ctx.drawImage(
-            cardimage,
-            dealerhandstartpoint + (cardsize.width * index),
-            anchors.dealer.posY,
-            cardsize.width,
-            cardsize.height,
-        )
-    }
-})
+drawgamestate()
